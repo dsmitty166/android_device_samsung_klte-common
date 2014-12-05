ad6555caaaf2cd28288fa99c74cf2d025ccb8552
Matt Filetto <matt.filetto@gmail.com>
2014-12-04 20:08:20 -0800

    add new camera prop
    Change-Id: I35ffe71257bd57e74460e0468dcc7bcf0b24a611

--- klte.mk
+++ klte.mk
@@ -124,5 +124,8 @@ PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
 # common msm8974
 $(call inherit-product, device/samsung/msm8974-common/msm8974.mk)
