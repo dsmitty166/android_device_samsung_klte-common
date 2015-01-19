d92e2c8b3e3da55eee10d4c440393c5e10f47cd3
Matt Filetto <matt.filetto@gmail.com>
2015-01-18 16:43:41 -0800

    Kang hlte sepolicy
    Change-Id: I258f06c3558482a331e71eda624d9b646e488c17

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,8 +79,9 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/klte-common/sepolicy
+BOARD_SEPOLICY_UNION += macloader.te
+BOARD_SEPOLICY_DIRS += device/samsung/hlte-common/sepolicy
+
 
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true
